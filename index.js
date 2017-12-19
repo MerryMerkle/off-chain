13acad5b10a549bb38895c7e0d3d4db13ebbeb1a
Matt Condon <mattgcondon@gmail.com>
2017-12-19 18:24:57 +0200

    fix: add start script

--- index.js
+++ index.js
@@ -125,6 +125,6 @@ announcer.io.on('connection', async (socket) => {
   })
 })
 
-http.listen(3000, function () {
+http.listen(process.env.PORT || 3000, function () {
   console.log('listening on *:3000')
 })
